32dce303a37ee6d6b02b880fd0b2fe65d193326e
NinaGallmetzer <nina.gallmetzer@stud.fh-campuswien.ac.at>
2025-02-18 13:54:12 +0100

    updated SeedDatabaseWorker.kt > add items to Room at App install TODO: - add & edit item

--- app/src/main/java/com/example/todoapp/workers/SeedDatabaseWorker.kt
+++ app/src/main/java/com/example/todoapp/workers/SeedDatabaseWorker.kt
@@ -2,17 +2,21 @@ package com.example.todoapp.workers
 
 import androidx.room.RoomDatabase
 import com.example.todoapp.data.ToDoDatabase
+import com.example.todoapp.data.daos.ItemDao
 import com.example.todoapp.data.daos.NoteDao
 import com.example.todoapp.data.models.enums.SyncType
+import com.example.todoapp.data.models.supabase.SupabaseItem
 import com.example.todoapp.data.models.supabase.SupabaseNote
 import com.example.todoapp.supabase
 import io.github.jan.supabase.postgrest.from
 
 class SeedDatabaseWorker : RoomDatabase.Callback() {
     private lateinit var noteDao: NoteDao
+    private lateinit var itemDao: ItemDao
 
     suspend fun seedDatabase(database: ToDoDatabase) {
         noteDao = database.noteDao()
+        itemDao = database.itemDao()
         seedDatabase()
     }
 
@@ -22,6 +26,12 @@ class SeedDatabaseWorker : RoomDatabase.Callback() {
             .decodeList<SupabaseNote>()
             .forEach { note ->
                 noteDao.add(note.toRoomNote(SyncType.synced))
+            }
+        supabase.from("item")
+            .select()
+            .decodeList<SupabaseItem>()
+            .forEach { item ->
+                itemDao.add(item.toRoomItem(SyncType.synced))
             }
     }
 
