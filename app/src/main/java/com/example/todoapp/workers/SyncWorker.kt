872fcb5b766c412395b20a0c6b9a25f32c6b5a2c
NinaGallmetzer <nina.gallmetzer@stud.fh-campuswien.ac.at>
2025-02-18 13:09:10 +0100

    ItemSync working !! on note delete > set items SyncStatus to delete not necessary > cascade in supabase TODO: - ItemScreen: add & edit item

--- app/src/main/java/com/example/todoapp/workers/SyncWorker.kt
+++ app/src/main/java/com/example/todoapp/workers/SyncWorker.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.example.todoapp.data.ToDoDatabase
+import com.example.todoapp.data.repositories.ItemRepository
 import com.example.todoapp.data.repositories.NoteRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -15,11 +16,13 @@ class SyncWorker(
 
     private val currentContext = context
     private val noteRepository = getNoteRepository(context)
+    private val itemRepository = getItemRepository(context)
 
     override suspend fun doWork(): Result {
         return withContext(Dispatchers.IO) {
             try {
                 noteRepository.syncNotes(context = currentContext)
+                itemRepository.syncItems(context = currentContext)
                 Result.success()
             } catch (e: Exception) {
                 Result.failure()
@@ -30,6 +33,11 @@ class SyncWorker(
     private fun getNoteRepository(context: Context): NoteRepository {
         return NoteRepository.getInstance(
             ToDoDatabase.getDatabase(context.applicationContext).noteDao(), context.applicationContext)
+    }
+
+    private fun getItemRepository(context: Context): ItemRepository {
+        return ItemRepository.getInstance(
+            ToDoDatabase.getDatabase(context.applicationContext).itemDao(), context.applicationContext)
     }
 
 }
